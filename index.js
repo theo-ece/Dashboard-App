44c918c26d56986bb85bd5ed1dce7d89ec861a7a
Nathan <nathan.lcmn@gmail.com>
2019-11-22 10:55:15 +0100

    Addition of CSS & metrics

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 express = require("express");
+metrics = require("./metrics");
 app = express();
 
 app.set("port", 1337);
 app.set("views", __dirname + "/view");
 app.set("view engine", "ejs");
 
+path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
+
 // app.get("/hello/:name", (req, res) => res.send("Hello " + req.params.name));
 app.get("/hello/:name", (req, res) =>
   res.render("hello.ejs", { name: req.params.name })
 );
+
+app.get("/metrics.json", (req, res) => {
+  metrics.get((err, data) => {
+    if (err) throw err;
+    res.status(200).json(data);
+  });
+});
 
 app.post("/", function(req, res) {
   // POST
