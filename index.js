6d085436ac1ab4b9aa157dffebad543e306f9713
Nathan <nathan.lcmn@gmail.com>
2019-11-22 10:19:11 +0100

    First working program
    Displays Hello [name]

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ express = require("express");
 app = express();
 
 app.set("port", 1337);
+app.set("views", __dirname + "/view");
+app.set("view engine", "ejs");
 
-app.get("/hello/:name", (req, res) => res.send("Hello " + req.params.name));
+// app.get("/hello/:name", (req, res) => res.send("Hello " + req.params.name));
+app.get("/hello/:name", (req, res) =>
+  res.render("hello.ejs", { name: req.params.name })
+);
 
 app.post("/", function(req, res) {
   // POST
